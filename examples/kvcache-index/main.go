56ed68e112a69600d9c37dfd5df4711d53db1e64
Maroon Ayoub <maroon.ayoub@ibm.com>
2025-04-30 12:57:18 +0300

    updated example

--- examples/kvcache-index/main.go
+++ examples/kvcache-index/main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"context"
+	"os"
 	"time"
 
-	"github.com/neuralmagic/distributed-kv-cache/pkg/kvcache"
-
-	"golang.org/x/net/context"
+	"github.com/neuralmagic/kvcache-manager/pkg/kvcache"
 	"k8s.io/klog/v2"
 )
 
@@ -13,19 +13,33 @@ import (
 Refer to docs/phase1-setup.md
 
 In Redis:
-1) "vllm@<modelName>@1@0@968e5b25ee324e1207bb88f4b4ad208731cbff985ac3ec6ce452b70aa54df4b4"
-2) "vllm@<modelName>@1@0@a9d880fb27ad4f27cd8affaf3607796ccb4f09bd2658b736766d0452c612cc93"
+1) "meta-llama/Llama-3.1-8B-Instruct@33c26f4ed679005e733e382beeb8df69d8362c07400bb07fec69712413cb4310"
+2) "meta-llama/Llama-3.1-8B-Instruct@0a3fd4e370c8aa0fafea88040e14f08aace073029aeec81a2b3aa8be8b84d6ae"
+2) "mistralai/Mistral-7B-Instruct-v0.2@923cdf5f667a7c3e059a1f7b8ed8b7e61d079a1bdceb47196575f4c327a674ae"
+3) "mistralai/Mistral-7B-Instruct-v0.2@e59c0c9babc978ec7d1f22510c7c3cae345f49fe88497c49ae598b95ee948313"
 */
 
 //nolint:lll // need prompt as-is, chunking to string concatenation is too much of a hassle
 const prompt = `lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum. Pellentesque malesuada nulla a mi. Duis sapien sem, aliquet nec, commodo eget, consequat quis, neque. Aliquam faucibus, elit ut dictum aliquet, felis nisl adipiscing sapien, sed malesuada diam lacus eget erat. Cras mollis scelerisque nunc. Nullam arcu. Aliquam consequat. Curabitur augue lorem, dapibus quis, laoreet et, pretium ac, nisi. Aenean magna nisl, mollis quis, molestie eu, feugiat in, orci. In hac habitasse platea dictumst.`
-const modelName = "mistralai/Mistral-7B-Instruct-v0.2"
+const modelName = "ibm-granite/granite-3.3-8b-instruct"
+
+func getKVCacheIndexerConfig() *kvcache.Config {
+	config := kvcache.NewDefaultConfig()
+
+	// For sample running with mistral (tokenizer), a huggingface token is needed
+	huggingFaceToken := os.Getenv("HF_TOKEN")
+	if huggingFaceToken != "" {
+		config.TokenizersPoolConfig.HFTokenizerConfig.HuggingFaceToken = huggingFaceToken
+	}
+
+	return config
+}
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 	logger := klog.FromContext(ctx)
 
-	kvCacheIndexer, err := kvcache.NewKVCacheIndexer(kvcache.NewDefaultConfig())
+	kvCacheIndexer, err := kvcache.NewKVCacheIndexer(getKVCacheIndexerConfig())
 	if err != nil {
 		logger.Error(err, "failed to init Indexer")
 	}
